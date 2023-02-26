597847354dd7ce589b0cfc6c816889be61707f3a
Vincode91 <vincenzatto@gmail.com>
2023-02-26 17:04:44 +0100

    adding POJOS. Closes #2

---
+++ src/main/java/guru/springframework/sfgtrainingpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package guru.springframework.sfgtrainingpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/guru/springframework/sfgtrainingpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package guru.springframework.sfgtrainingpetclinic.model;
+
+public class Vet extends Person{
+}
