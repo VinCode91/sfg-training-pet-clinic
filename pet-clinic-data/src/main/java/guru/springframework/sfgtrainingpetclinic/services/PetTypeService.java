eb75953d12a94013a9ef6495d4423b961ad48503
Vincode91 <vincenzatto@gmail.com>
2023-03-12 19:59:41 +0100

    added pet type map service, closes #27

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgtrainingpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgtrainingpetclinic.services;
+
+import guru.springframework.sfgtrainingpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
