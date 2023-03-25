fa11d783b460158f61e477466be0093217b61c4d
Vincode91 <vincenzatto@gmail.com>
2023-03-25 13:08:37 +0100

    vet spring data jpa service, closes #38

--- pet-clinic-data/src/main/java/guru/springframework/sfgtrainingpetclinic/repositories/VetRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgtrainingpetclinic/repositories/VetRepository.java
@@ -4,4 +4,5 @@ import guru.springframework.sfgtrainingpetclinic.model.Vet;
 import org.springframework.data.repository.CrudRepository;
 
 public interface VetRepository extends CrudRepository<Vet, Long> {
+    Vet findByLastName(String lastName);
 }
