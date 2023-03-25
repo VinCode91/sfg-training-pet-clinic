ead0e4183e6fc536396f9188a5577c7821114c78
Vincode91 <vincenzatto@gmail.com>
2023-03-25 14:06:31 +0100

    add visit map service, closes #43

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgtrainingpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgtrainingpetclinic.services;
+
+import guru.springframework.sfgtrainingpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
